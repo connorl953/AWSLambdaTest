d1a99488f39df7c9a9187ba6ad5f30e9c61b1707
connorl953 <connor009988@gmail.com>
2024-03-10 00:40:46 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask
 import time
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.event_handler.api_gateway import ApiGatewayResolver
 
 app = Flask(__name__)
+app_resolver = ApiGatewayResolver(app)
 
 @app.route('/')
 def hello_world():
-    time.sleep(5)  # Wait for 5 seconds
+    time.sleep(5)  # Simulate delay
     return 'Hello World'
+
+@ApiGatewayResolver.get("/")  # Decorator to create Lambda handler
+def lambda_handler(event: dict, context: LambdaContext):
+    return app_resolver.resolve(event, context)
 
 if __name__ == '__main__':
     app.run(debug=True)
